32740c613dd4c9b54e2651f51000ee3dd2a7893b
helmax-y <max_rad@ukr.net>
2019-12-06 17:32:21 +0200

    Updates

---
+++ precache-manifest.adeaaba94b2009ea17701d49e5468b68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10bd0743d62fc2bf91c0b478a40aa20b",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "df64d6fd25bd3c3e6dc7",
+    "url": "/react_tabs/static/css/main.e454bf03.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/react_tabs/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "df64d6fd25bd3c3e6dc7",
+    "url": "/react_tabs/static/js/main.017ac761.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
